4beaf16fea2532d90614b818818e14fac454830f
HarryXiong24 <harryxiong24@gmail.com>
2021-12-02 22:40:00 +0800

    feat: add tabs

---
+++ src/components/Tabs/tabItem.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import './style.scss';
+import '@/styles';
+
+export interface TabItemProps {
+  /** Tab选项上面的文字 */
+  label: string | React.ReactElement;
+  /** Tab选项是否被禁用 */
+  disabled?: boolean;
+}
+
+const TabItem: FC<TabItemProps> = (props) => {
+  const { children } = props;
+
+  return <>{children}</>;
+};
+
+TabItem.displayName = 'TabItem';
+
+export default TabItem;
--- src/pages/Card/index.tsx
+++ src/pages/Card/index.tsx
@@ -1,77 +1,19 @@
 import React from 'react';
-import Card from '@/components/Card';
-import Button from '@/components/Button';
-import ReactMarkdown from 'react-markdown';
-import rehypeHighlight from 'rehype-highlight';
-import remarkGfm from 'remark-gfm';
-import CardMD from './Card.md';
-import '@/styles/example.scss';
+import CardDemo from './demo';
+import CardDoc from './doc';
+import Tabs from '@/components/Tabs/tabs';
+import TabItem from '@/components/Tabs/tabItem';
 
 const CardPage = () => {
-  const cardCss = { margin: '20px 20px 0 0' };
   return (
-    <>
-      <h2>Card 卡片</h2>
-      <div className="container">
-        <Card
-          title="基本使用"
-          shadow
-          style={cardCss}
-          titleOverflowHidden={false}
-          // cover={Img}
-        >
-          <h1>I am Card!</h1>
-        </Card>
-        <Card
-          title="title 右边自定义 Node 元素"
-          shadow
-          style={cardCss}
-          extra={
-            <div>
-              <Button
-                btnType="ghost"
-                onClick={() => {
-                  alert('I was clicked!');
-                }}
-              >
-                按钮
-              </Button>
-            </div>
-          }
-        >
-          <h1>I am Card!</h1>
-        </Card>
-        <Card
-          title="卡片带按钮"
-          shadow
-          actions={[
-            {
-              name: '按钮1',
-              event: () => {
-                alert('I was clicked!');
-              },
-            },
-            {
-              name: '按钮2',
-            },
-          ]}
-          style={cardCss}
-        >
-          <h1>I am Card!</h1>
-        </Card>
-        <Card title="悬浮出现卡片阴影" showShadowWhenHover style={cardCss}>
-          <h1>I am Card!</h1>
-        </Card>
-      </div>
-      <Card shadow style={{ marginTop: 50 }}>
-        <ReactMarkdown
-          rehypePlugins={[rehypeHighlight]}
-          remarkPlugins={[remarkGfm]}
-          // eslint-disable-next-line react/no-children-prop
-          children={CardMD}
-        />
-      </Card>
-    </>
+    <Tabs type="line" defaultIndex={0}>
+      <TabItem label="Demo">
+        <CardDemo />
+      </TabItem>
+      <TabItem label="API">
+        <CardDoc />
+      </TabItem>
+    </Tabs>
   );
 };
 
