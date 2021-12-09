4b94ed6c69ece1d7d775261b1225e7044eed1352
harryxiong <harryxiong24@gmail.com>
2021-12-09 11:34:03 +0800

    chore: add creating component file automatically

--- scripts/plopfile.js
+++ scripts/plopfile.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = function (plop) {
   plop.setGenerator('page_template', {
-    description: 'create new files',
+    description: 'create a new component demo page',
     prompts: [
       {
         type: 'input',
         name: 'name',
-        message: 'please enter new component demo page',
+        message: 'please enter a new component demo page name',
       },
     ],
     actions: [
@@ -53,6 +53,52 @@ module.exports = function (plop) {
         templateFile: path.resolve(
           __dirname,
           '../templates/page_template/md.hbs'
+        ),
+      },
+    ],
+  });
+
+  plop.setGenerator('component_template', {
+    description: 'create a new component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'please enter a new component name',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: path.resolve(
+          __dirname,
+          '../src/components/{{pascalCase name}}/index.tsx'
+        ),
+        templateFile: path.resolve(
+          __dirname,
+          '../templates/component_template/index.hbs'
+        ),
+      },
+      {
+        type: 'add',
+        path: path.resolve(
+          __dirname,
+          '../src/components/{{pascalCase name}}/type.ts'
+        ),
+        templateFile: path.resolve(
+          __dirname,
+          '../templates/component_template/type.hbs'
+        ),
+      },
+      {
+        type: 'add',
+        path: path.resolve(
+          __dirname,
+          '../src/components/{{pascalCase name}}/style.scss'
+        ),
+        templateFile: path.resolve(
+          __dirname,
+          '../templates/component_template/style.hbs'
         ),
       },
     ],
